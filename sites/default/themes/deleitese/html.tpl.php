ac134649afe74bc94529a10b7e321934c2f75355
John serrano <andrey_201015@hotmail.com>
2017-07-24 09:29:40 -0500

    Cambiando script de mapa de calor

--- sites/default/themes/deleitese/html.tpl.php
+++ sites/default/themes/deleitese/html.tpl.php
@@ -123,12 +123,12 @@
 
   <!-- Mapa de calor -->
   <script type="text/javascript">
-    window.smartlook || (function (d) {
-    var o = SmartLook = function () {o.api.push (argumentos)}, h = d.getElementsByTagName ( 'cabeza') [0];
-    var c = d.createElement ( 'script'); o.api = new Array (); c.async = true; c.type = 'text / javascript';
-    c.charset = 'utf-8'; c.src = 'https: //rec.smartlook.com/recorder.js'; h.appendChild (c);
-    })(documento);
-    SmartLook ( 'init', 'a2a2341660585b59f5d75fb87281b63346e5e51c');
+    window.smartlook||(function(d) {
+    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+    c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
+    })(document);
+    smartlook('init', 'a2a2341660585b59f5d75fb87281b63346e5e51c');
   </script>
   <!-- Fin mapa de calor -->
 
