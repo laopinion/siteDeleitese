08fba8d80e7d654bab3cf1a6b63b93f1087ce554
Jhonatan <jhonatan728@gmail.com>
2018-09-05 09:39:39 -0500

    Añadiendo boletin a landing

--- sites/default/themes/deleitese/page--node--1215.tpl.php
+++ sites/default/themes/deleitese/page--node--1215.tpl.php
@@ -1,3 +1,51 @@
+<div class="emergentes">
+  <div class="content">
+    <div class="newsletter">
+      <h3>Suscríbete para recibir nuestro boletín</h3>
+      <form id="newsletterForm">
+        <input type="text" name="fname" placeholder="Nombre y apellido" required />
+        <input type="email" name="email" placeholder="Correo electrónico" required />
+        <div class="terminos">
+          <input type="radio" name="terminos" value="Acepta terminos y condiciones" required>
+          <label class="option" for="edit-submitted-terminos-y-condiciones-de-uso-1">Acepto la <a href="http://www.deleitese.co/politica-de-tratamiento-de-datos-personales" target="_blank"> política de tratamiento de los datos personales</a> </label>
+        </div>
+        <div class="form-actions">
+          <input type="submit" name="enviar" value="Enviar">
+        </div>
+      </form>
+    </div>
+    <!--Rutas--><?php $view = views_get_view('detalle'); print $view->preview('block_7'); ?><!--FIN Rutas-->
+    <div class="pauta p300x250"><div class="info">
+      <script async type='text/javascript' src='//s.clickiocdn.com/t/common_258.js'></script>
+      <div class='__lxGc__'></div>
+      <script type='text/javascript'>
+      ((__lxGc__=window.__lxGc__||{'s':{},'b':0})['s']['_200905']=__lxGc__['s']['_200905']||{'b':{}})['b']['_583726']={'i':__lxGc__.b++};
+      </script>
+      </div>
+    </div>
+    <!--Donde--><?php // $view = views_get_view('detalle'); print $view->preview('block_7'); ?><!--FIN Donde-->
+  </div>
+</div>
+
+<div class="newsletter" style="display: none;">
+  <h3>Suscríbete para recibir nuestro boletín</h3>
+  <form id="newsletterForm">
+    <input type="text" name="fname" placeholder="Nombre y apellido" required />
+    <input type="email" name="email" placeholder="Correo electrónico" required />
+    <div class="terminos">
+      <input type="radio" name="terminos" value="Acepta terminos y condiciones" required>
+      <label class="option" for="edit-submitted-terminos-y-condiciones-de-uso-1">
+        <a href="http://www.deleitese.co/politica-de-tratamiento-de-datos-personales">
+          Acepto términos y condiciones
+        </a>
+      </label>
+    </div>
+    <div class="form-actions">
+      <input type="submit" name="enviar" value="Enviar">
+    </div>
+  </form>
+</div>
+
 <div id="hamburguesaton">
     <div class="contenido">
         <div class="hamburguesaton"><img src="<?php print base_path(); ?>sites/default/themes/deleitese/images/hamburguesaton/logo_h.png"></div>
