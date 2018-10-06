e0314089f79f8d297616785f57e0339408eb1cc2
Jhonatan <jhonatan728@gmail.com>
2018-10-06 08:58:19 -0500

    Actualizando TPL Hamburguesaton

--- sites/default/themes/deleitese/page--node--1215.tpl.php
+++ sites/default/themes/deleitese/page--node--1215.tpl.php
@@ -49,8 +49,9 @@
 -->
 <div id="hamburguesaton">
     <div class="contenido">
+    	<!--
     	<a class="typeform-share button" href="https://laopinion.typeform.com/to/cC4gMP" data-mode="popup" style="display:inline-block;text-decoration:none;background-color:#F7931E;color:white;cursor:pointer;font-family:Helvetica,Arial,sans-serif;font-size:20px;line-height:50px;text-align:center;margin:0;height:50px;padding:0px 33px;border-radius:25px;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:bold;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;" target="_blank">¡Votar! </a> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
-
+		-->
         <div class="hamburguesaton"><img src="<?php print base_path(); ?>sites/default/themes/deleitese/images/hamburguesaton/logo_h.png"></div>
         <div class="que">
         	<img src="<?php print base_path(); ?>sites/default/themes/deleitese/images/hamburguesaton/que.png">
